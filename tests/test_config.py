17718c1667bade18d40c99a07fc8f918e15fad78
ahmed14-cell <ahmedito14@gmail.com>
2021-02-15 14:40:35 +0300

    test config.py params

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,6 @@
 import unittest
+from loan.config import Config
+from pathlib import Path
 
 
 class TestConfig(unittest.TestCase):
@@ -7,3 +9,35 @@ class TestConfig(unittest.TestCase):
     def test_random_state(self):
         con = Config()
         self.assertEqual(con.random_state, 42)
+    
+
+    def test_assets_path(self):
+        con = Config()
+        self.assertEqual(con.assets_path, Path('./loan_assets'))
+
+    
+    def test_original_data_path(self):
+        con = Config()
+        self.assertEqual(
+            con.original, Path('./loan_assets/loan_original/loan.xlsx')
+        )
+
+    
+    def test_splitted_dataset_path(self):
+        con = Config()
+        self.assertEqual(con.data, Path('./loan_assets/loan_data'))
+
+    
+    def test_features_path(self):
+        con = Config()
+        self.assertEqual(con.features, Path('./loan_assets/loan_features'))
+
+    
+    def test_models_path(self):
+        con = Config()
+        self.assertEqual(con.models, Path('./loan_assets/loan_models'))
+
+
+    def test_metrics_path(self):
+        con = Config()
+        self.assertEqual(con.metrics, Path('./loan_assets/loan_metrics.json'))
