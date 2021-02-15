02e16b36f37d5befc05171e36bd89f0e480a8fb9
ahmed14-cell <ahmedito14@gmail.com>
2021-02-15 14:41:23 +0300

    update config params

---
+++ loan/config.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+
+class Config:
+    random_state=42
+    assets_path = Path('./loan_assets')
+    original = assets_path / 'loan_original' / 'loan.xlsx'
+    data = assets_path / 'loan_data'
+    features = assets_path / 'loan_features'
+    models = assets_path / 'loan_models'
+    metrics =  assets_path / 'loan_metrics.json'
