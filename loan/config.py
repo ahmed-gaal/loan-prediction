3d5d11e08719efaa26a2b54d83b6beaeb1e068a5
ahmed14-cell <ahmedito14@gmail.com>
2021-02-17 08:35:42 +0300

    update config params

--- loan/config.py
+++ loan/config.py
@@ -4,7 +4,7 @@ from pathlib import Path
 class Config:
     random_state=42
     assets_path = Path('./loan_assets')
-    original = assets_path / 'loan_original' / 'loan.csv'
+    original = assets_path / 'loan_original' / 'clean_loan.csv'
     data = assets_path / 'loan_data'
     features = assets_path / 'loan_features'
     models = assets_path / 'loan_models'
