b1030d6389b8a3349e594e549141548ed0693d71
ahmed14-cell <ahmedito14@gmail.com>
2021-02-20 14:52:32 +0300

    increase C param.

--- loan/model_training.py
+++ loan/model_training.py
@@ -12,7 +12,7 @@ X_train = Workflow.load_df(Config.features, data='train_features.csv')
 y_train = Workflow.load_df(Config.features, data='train_target.csv')
 
 # Instantiating model algorithm
-clf = SVC(C=1.0, kernel='rbf', gamma='scale')
+clf = SVC(C=1.01, kernel='rbf', gamma='scale')
 
 # Fitting features to the algorithm
 clf.fit(X_train, y_train.to_numpy().ravel())
